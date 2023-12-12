e03d395b6e45af261a514b943465e63d4c8d13d1
Ashish Kalaria <131858574+AshishKalaria@users.noreply.github.com>
2023-12-12 15:04:17 +0530

    connect to mongodb

--- api/index.js
+++ api/index.js
@@ -1,4 +1,7 @@
 import express from "express";
+import mongoose from "mongoose";
+import * as dotenv from "dotenv";
+dotenv.config();
 const app = express();
 const port = 4000;
 
@@ -6,6 +9,20 @@ app.get("/", (req, res) => {
 	res.send("<h1>Welcome to Home Page</h1>");
 });
 
-app.listen(port, () => {
-	console.log(`Server is running on port ${port}...`);
+app.get("*", (req, res) => {
+	res.sendFile(path.resolve(__dirname, "./client", "index.html"));
 });
+
+app.use("*", (req, res) => {
+	res.status(404).json({ msg: "not found" });
+});
+
+try {
+	await mongoose.connect(process.env.MONGO_URI);
+	app.listen(port, () => {
+		console.log(`server running on PORT ${port}....`);
+	});
+} catch (error) {
+	console.log(error);
+	process.exit(1);
+}
