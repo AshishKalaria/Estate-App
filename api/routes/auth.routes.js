c160aea983eeeb4cdb329e1f5e8161a68a42e085
Ashish Kalaria <131858574+AshishKalaria@users.noreply.github.com>
2023-12-13 15:49:49 +0530

    create and add OAuth component

--- api/routes/auth.routes.js
+++ api/routes/auth.routes.js
@@ -1,8 +1,14 @@
 import { Router } from "express";
 const router = Router();
-import { signup, signin } from "../controller/auth.controller.js";
+import {
+	signup,
+	signin,
+	google,
+	signOut,
+} from "../controller/auth.controller.js";
 
 router.post("/signup", signup);
 router.post("/signin", signin);
-
+router.post("/google", google);
+router.get("/signout", signOut);
 export default router;
